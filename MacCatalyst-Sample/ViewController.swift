3fb6f2587d0415a8e47db89b3279e561c78d75f7
Reon0429-cat <oonishireon110@gmail.com>
2021-10-27 06:09:41 +0900

    first commit

---
+++ MacCatalyst-Sample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacCatalyst-Sample
+//
+//  Created by 大西玲音 on 2021/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var countButton: UIButton!
+    var count = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = String(count)
+    }
+
+    @IBAction func countButtonDidTapped(_ sender: Any) {
+        count += 1
+        label.text = String(count)
+    }
+    
+}
+
